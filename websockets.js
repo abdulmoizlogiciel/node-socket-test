30d33999596cf15f34f935660fbf70d761f7498d
abdulmoizlogiciel <abdul.moiz@logicielservice.com>
2022-04-20 15:36:43 +0500

    REFACTORING.

--- websockets.js
+++ websockets.js
@@ -1,9 +1,11 @@
 
+const WebSocket = require('ws');
 const { baseUrl } = require("./config.json");
 const tokens = require("./tokens.json");
-const WebSocket = require('ws');
 
 const username = process.argv[2] || "user1";
+const socketRoute = process.argv[3] || "user1";
+
 const access_token = tokens[username];
 
 // hand shaking
@@ -12,10 +14,6 @@ const websocket = new WebSocket(`ws://${baseUrl}/ws?access_token=` + access_toke
 // on connected
 websocket.onopen = function (e) {
     console.log("onopen: Connected");
-
-    // https://stackoverflow.com/questions/41074052/how-to-terminate-a-websocket-connection
-    // websocket.close(); // soft close a connection.
-    // websocket.terminate(); // hard close a connection.
 };
 
 // on closed
@@ -26,7 +24,6 @@ websocket.onclose = function (e) {
 // on message received
 websocket.onmessage = function (e) {
     console.log("onmessage:", JSON.parse(e.data)?.msg);
-    // console.log("message", JSON.stringify(e), e.Symbol, count++);
 };
 
 // on error
@@ -34,21 +31,7 @@ websocket.onerror = function (e) {
     console.log("onerror", e.message);
 };
 
-function heartbeat() {
-    clearTimeout(this.pingTimeout);
-
-    // Use `WebSocket#terminate()`, which immediately destroys the connection,
-    // instead of `WebSocket#close()`, which waits for the close timer.
-    // Delay should be equal to the interval at which your server
-    // sends out pings plus a conservative assumption of the latency.
-    this.pingTimeout = setTimeout(() => {
-      websocket.terminate();
-    }, 30000 + 1000);
-    console.log("inside hearbeat");
-}
-websocket.on('ping', heartbeat);
-
-// setTimeout(() => {
-//   websocket.close();
-//   // websocket.terminate();
-// }, 10000);
+// To close a socket connection:
+// https://stackoverflow.com/questions/41074052/how-to-terminate-a-websocket-connection
+// websocket.close(); // soft close a connection.
+// websocket.terminate(); // hard close a connection.
